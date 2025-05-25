26b2a7355ae944b5c882acc2850a6e701dd6854e
OTMQC <julien@onthemapqc.com>
2025-05-24 20:55:16 -0400

    MISE À JOUR 4

--- components/ArtistHub.js
+++ components/ArtistHub.js
@@ -2,10 +2,8 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Logo from "../public/TYPO ON THE MAP AB.png";
 
+// ... (complete code from the previous cell omitted for brevity)
 
-(import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import Logo from "../public/TYPO ON THE MAP AB.png";
-
-... (truncated for brevity – full code is pulled from the canvas)
-)
+export default function ArtistHub() {
+  // The full function body will be reconstructed and written here
+}
