6b2332dd7f495c9c0dc7598c73a3c9a6fb032954
OTMQC <julien@onthemapqc.com>
2025-05-24 20:23:49 -0400

    MISE À JOUR $

--- components/ArtistHub.js
+++ components/ArtistHub.js
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import Logo from "../public/TYPO ON THE MAP AB.png";
+
 
 (import React, { useState, useEffect } from "react";
 import Image from "next/image";
