c03346c3af8e5cffe0db57de32b66d62c533d980
OTMQC <julien@onthemapqc.com>
2025-06-09 22:29:29 -0400

    Changement firebase

--- components/firebase.js
+++ components/firebase.js
@@ -1,4 +1,5 @@
-import { initializeApp } from "firebase/app";
+
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -8,10 +9,10 @@ const firebaseConfig = {
   storageBucket: "otmqc-hub.firebasestorage.app",
   messagingSenderId: "1049045457405",
   appId: "1:1049045457405:web:aae3c7e6d408aac945e194",
-  measurementId: "G-HS19V82PYP"
+  measurementId: "G-HS19V82PYP",
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 
 export { db };
